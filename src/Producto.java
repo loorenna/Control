9126cc14b120f36f8591ed9fdb9708b7380eafc1
loorenna <lorenaalisson2004@gmail.com>
2026-04-18 17:17:21 -0500

    Agregado descuento condicional del 10%

--- src/Producto.java
+++ src/Producto.java
@@ -14,11 +14,17 @@ public class Producto {
     }
     public void mostrarFactura() {
         double total = precio * cantidad;
+        double descuento = 0;
+        if (total > 1000) {
+            descuento = total * 0.10;
+        }
+        double totalPagar = total - descuento;
         System.out.println("\n----------- FACTURA -----------\n");
         System.out.println("Producto: " + nombre);
         System.out.println("Precio unitario: " + precio);
         System.out.println("Cantidad: " + cantidad);
-        System.out.println("\nTotal a pagar: " + total);
-
+        System.out.println("Subtotal: " + total);
+        System.out.println("Descuento: " + descuento);
+        System.out.println("\nTotal a pagar: " + totalPagar);
     }
 }
