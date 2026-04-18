735ccea0858fc87b712a21f4cc09c5387cd32b4a
loorenna <lorenaalisson2004@gmail.com>
2026-04-18 17:11:22 -0500

    Realización del ejercicio2 sobre factura de un producto

---
+++ src/Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Producto p = new Producto();
+        p.ingresarDatos();
+        p.mostrarFactura();
+    }
+}
---
+++ src/Producto.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Producto {
+    String nombre;
+    double precio;
+    int cantidad;
+    public void ingresarDatos() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el nombre del producto: ");
+        nombre = sc.nextLine();
+        System.out.print("Ingrese el precio: ");
+        precio = sc.nextDouble();
+        System.out.print("Ingrese la cantidad: ");
+        cantidad = sc.nextInt();
+    }
+    public void mostrarFactura() {
+        double total = precio * cantidad;
+        System.out.println("\n----------- FACTURA -----------\n");
+        System.out.println("Producto: " + nombre);
+        System.out.println("Precio unitario: " + precio);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("\nTotal a pagar: " + total);
+    }
+}
