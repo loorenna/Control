63d09e701841f76041057665379b0c6d7a4a87be
loorenna <lorenaalisson2004@gmail.com>
2026-04-18 16:45:09 -0500

    Realización del ejercicio1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,7 @@
+public class Ejercicio1 {
+    public  static  void main(String[] args){
+        Estudiante est = new Estudiante();
+        est.registrarDatos();   // ingresar datos
+        est.mostrarDatos();
+    }
+}
